5d182308516f08512c8626744d488564cd415139
Andrea Vannini <anvannin@c2r5p8.42firenze.it>
2022-10-29 18:59:55 +0200

    .

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,6 +1,12 @@
-#include "../libft/libft.h"
 #include <unistd.h>
 #include <stdarg.h>
+
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr(const char *str)
 {
@@ -8,7 +14,78 @@ void	ft_putstr(const char *str)
 
     i = 0;
 	while (str[i] != '\0')
-        write(1, &str[i++], 1);
+        	ft_putchar(str[i++]);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + 48);
+}
+
+void	ft_putunsign_nbr(unsigned int nb)
+{
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + 48);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strev(char *str)
+{
+	int	i;
+	int	len;
+	char	temp;
+
+	i = 0;
+	len = ft_strlen(str);
+	while (i < len/2)
+	{
+		temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+		i++;
+	}
+	return (str);
+}
+
+void	ft_putnbr_hex(int nb)
+{
+	int	i;
+	int	temp;
+	char	hex[20];
+	
+	i = 0;
+	while(nb > 9)
+	{
+		temp = nb % 16;
+		if (temp < 10)
+			temp += 48;
+		else
+			temp += 55;
+		hex[i++] = temp;
+		nb /= 16;
+	}
+	hex[i++] = nb + 48;
+	hex[i] = '\0';
+	ft_putstr(hex);
+	write(1, &"\n", 1);
+	ft_putstr(ft_strev(hex));
 }
 
 void    ft_conversions_check(char c, void *var)
@@ -40,45 +117,80 @@ int    ft_check_perc(const char *str)
 
 void    ft_printf(const char *str, ...)
 {
-    int         i;
-    va_list     ptr;
-    va_start    (ptr, str);
+	int	i;
+	va_list	ptr;
 
-    i = 0;
-    if (ft_check_perc(str) == 1)
-    {
-        while (str[i])
-        {
-            if(str[i] == '%' && str[i + 1] == '%')
-                i++;
-            // else if (str[i] == '%' && str[i + 1] != '%')
-            //     ft_conversions_check(str[i++], va_arg(ptr, void));
-            write(1, &str[i], 1);
-            i++;
-        }        
-    }
-    else
-        ft_putstr(str);
+	i = 0;
+	va_start(ptr, str);
+	if (ft_check_perc(str) == 1)
+	{
+		while (str[i])
+		{
+			if(str[i] == '%' && str[i + 1] == '%')
+				i++;
+			// else if (str[i] == '%' && str[i + 1] != '%')
+			//     ft_conversions_check(str[i++], va_arg(ptr, void));
+			write(1, &str[i], 1);
+			i++;
+		}        
+	}
+	else
+	ft_putstr(str);
 }
 
 #include <stdio.h>
+void	variadic_test(const char* str, ...)
+{
+	int	c;
+    	va_list ptr;
+
+	va_start(ptr, str);
+	while (*str != '\0')
+	{
+		if (*str == '%')
+		{
+			str++;
+			if (*str == 'd' || *str == 'i')
+				ft_putnbr(va_arg(ptr, int));
+			else if (*str == 'u')
+				ft_putunsign_nbr(va_arg(ptr, unsigned int));
+			else if (*str == 'c')
+				ft_putchar(va_arg(ptr, int));
+			else if(*str == 's')
+				ft_putstr(va_arg(ptr, char*));
+			//else if (*str == 'p')
+			//else if (*str == 'X')
+			else if (*str == 'x')
+				ft_putnbr_hex(va_arg(ptr, int));
+			else
+				str--;
+		}
+		else
+			write(1, &str, 1);
+		str++;
+	}
+	va_end(ptr);
+}
+
 int     main()
 {
-    char a = 'a';
     int n = 10;
     int *p = &n;
 
-    ft_printf("Hello world!\n");
-    ft_printf("One percent: %\n");
-    ft_printf("Two percent: %%\n");
+    // ft_printf("Hello world!\n");
+    // ft_printf("One percent: %\n");
+    // ft_printf("Two percent: %%\n");
 
-    // printf("%\n");
-
-
-    // write(1, &"\n", 1);
-    // ft_printf("%c", 'a');
-    // write(1, &"\n", 1);
-
+    variadic_test("%d", 42);
+    write(1, &"\n", 1);
+    variadic_test("%s", "Hello World\n");
+    variadic_test("%c", 'F');
+    write(1, &"\n", 1);
+    variadic_test("%u", 4294967295);
+    write(1, &"\n", 1);
+    variadic_test("%x", 1234);
+    write(1, &"\n", 1);
+    
     // printf("%%c: %c\n", a);
     // printf("%%s: %s\n", "Hello World");
     // printf("%%p: %p\n", p);
@@ -88,10 +200,4 @@ int     main()
     // printf("%%x: %x\n", n);
     // printf("%%X: %X\n", n);
     // printf("%%\n\n");
-
-    // printf("+123: %+d\n", +123);
-    // printf(" 123: % d\n", +123);
-    // printf("-123: %-d\n", -123);
-    // printf("a: %#x\n", 10);
-    // printf("A: %#X\n", 10);
 }
